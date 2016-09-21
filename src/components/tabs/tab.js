7c95c9dc1f60eb6a16d46bf3fff36cdfb6ba9a40
Ryan Heathcote <rheathcote@sharptop.co>
2016-09-21 17:58:52 -0600

    add followLink() method to tabs.

--- src/components/tabs/tab.js
+++ src/components/tabs/tab.js
@@ -1,14 +1,17 @@
-import {inlineView, bindable, containerless} from 'aurelia-framework'
+import {inlineView, bindable, containerless, inject} from 'aurelia-framework'
+import {Router} from 'aurelia-router'
 
 @containerless()
 @inlineView(`
 <template>
-    <a class="tab-link" href.bind="href" class.bind="isActive ? 'active' : ''">
+    <a class="tab-link" href.bind="href" class.bind="isActive ? 'active' : ''" click.trigger="followLink()">
         <i class="fa" class.bind="icon ? 'fa-' + icon : ''"></i>
         <span class="tabbar-label" innerHtml.bind="label"></span>
     </a>
 </template>
 `)
+
+@inject(Router)
 export class Tab {
 
     @bindable label = ''
@@ -16,4 +19,11 @@ export class Tab {
     @bindable icon = ''
     @bindable isActive = false
 
+    constructor(router) {
+        this.router = router
+    }
+
+    followLink() {
+        this.router.navigate(this.href)
+    }
 }
