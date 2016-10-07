b0c0b7eb1202fa184f5ff7979ffd79bdeda2e455
Ryan Heathcote <rheathcote@sharptop.co>
2016-10-07 17:30:11 -0400

    Adjustments to the card class to enable an arrow on the side.

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,13 +3,25 @@ import {inlineView, bindable, containerless} from 'aurelia-framework'
 @containerless()
 @inlineView(`
 <template>
-    <div class="card" class.bind="fullHeight ? 'full-height' : ''">
-        <slot></slot>
+    <div class="card" class.bind="cssClasses">
+        <div class="card-body">
+            <slot></slot>
+        </div>
+        <i if.bind="arrow" class="card-arrow fa" class.bind="arrowClass"></i>
     </div>
 </template>
 `)
 export class Card {
 
     @bindable fullHeight
+    @bindable arrow = ''
+
+    get cssClasses() {
+        return `${this.fullHeight ? 'full-height' : ''} ${this.arrow ? `arrow arrow-${this.arrow}` : ''}`
+    }
+
+    get arrowClass() {
+        return `fa-arrow-${this.arrow}`
+    }
 
 }
