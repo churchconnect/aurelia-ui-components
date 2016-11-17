73e012a75a60931e80dc00ee30308e741a44b280
Ryan Heathcote <rheathcote@sharptop.co>
2016-11-17 13:10:31 -0500

    basic share link component

---
+++ src/components/card/share-link.js
@@ -0,0 +1,31 @@
+import {inlineView, containerless, bindable} from 'aurelia-framework'
+
+@containerless()
+@inlineView(`
+<template>
+    <i class="fa fa-share-alt share-link" aria-hidden="true" click.delegate="share()"></i>
+</template>
+`)
+export class ShareLink {
+    @bindable sharingInfo
+
+    share() {
+        if (typeof cordova === 'undefined') {
+            console.log('running outside of cordova: sharing disabled')
+            console.log(this.sharingInfo)
+        } else {
+            var onSuccess = function (result) {
+                console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+                console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+            }
+
+            var onError = function (msg) {
+                console.log("Sharing failed with message: " + msg);
+            }
+
+            window.plugins.socialsharing.shareWithOptions(this.sharingInfo, onSuccess, onError);
+        }
+    }
+
+}
+
