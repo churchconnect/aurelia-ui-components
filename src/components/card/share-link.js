074c6a2729cdbeec7b757dcc8987a9e2ae100d86
Ryan Heathcote <rheathcote@sharptop.co>
2016-11-19 15:41:51 -0500

    fix sharing component to use link from contentful as url for plugin.

--- src/components/card/share-link.js
+++ src/components/card/share-link.js
@@ -14,6 +14,12 @@ export class ShareLink {
             console.log('running outside of cordova: sharing disabled')
             console.log(this.sharingInfo)
         } else {
+            var options = {
+                message: this.sharingInfo.message,
+                subject: this.sharingInfo.title,
+                url: this.sharingInfo.link
+            }
+
             var onSuccess = function (result) {
                 console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
                 console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
@@ -23,7 +29,7 @@ export class ShareLink {
                 console.log("Sharing failed with message: " + msg);
             }
 
-            window.plugins.socialsharing.shareWithOptions(this.sharingInfo, onSuccess, onError);
+            window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
         }
     }
 
